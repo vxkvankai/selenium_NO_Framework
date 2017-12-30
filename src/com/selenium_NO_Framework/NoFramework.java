04fff306beadd5a3d4ea9ebe579e518dc5a85cb5
herbert scruggs <herbscruggs@gmail.com>
2017-12-30 11:22:12 -0600

    updated the project and included a .gitignore to not check in binaries produced.  Checked in a lib dir which is bad, but decided it best for training for now.

--- src/com/selenium_NO_Framework/NoFramework.java
+++ src/com/selenium_NO_Framework/NoFramework.java
@@ -1,27 +1,24 @@
 package com.selenium_NO_Framework;
 
-
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
 import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.internal.FindsByLinkText;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class NoFramework {
-    System.setProperty("webdriver.chrome.driver", "/Users/vamsik/Documents/workspace/chromedriver");
-    
-    WebDriver driver = new ChromeDriver();
+
+    WebDriver driver;
 
     @BeforeTest
     public void setUp() {
-
-
+        System.setProperty("webdriver.chrome.driver", "libs/chromedriver"); //need to define where the exe is
+        driver = new ChromeDriver();
         driver.get("http://book.theautomatedtester.co.uk/");
     }
 
@@ -31,11 +28,15 @@ public class NoFramework {
     }
 
     @Test
-    public void testEcamplesGoHere() {
+    public void testEcamplesGoHere() throws InterruptedException {
+
+        sleep(5000); //I just put a sleep method to slow things down to see things happen
         List<WebElement> elements = ((FindsByLinkText) driver).findElementsByLinkText("Chapter1");
         Assert.assertEquals(1, elements.size());
     }
 
-
+    private void sleep(int time) throws InterruptedException {
+        Thread.sleep(time);
+    }
 
 }
