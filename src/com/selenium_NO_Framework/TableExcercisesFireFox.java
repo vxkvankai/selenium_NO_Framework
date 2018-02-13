6916dd2e3b2fb3cfb95fa8dc423a45d7957a6562
herbert scruggs <herbscruggs@gmail.com>
2018-02-12 20:31:01 -0600

    fixed to run firefox.

--- src/com/selenium_NO_Framework/TableExcercisesFireFox.java
+++ src/com/selenium_NO_Framework/TableExcercisesFireFox.java
@@ -2,17 +2,39 @@ package com.selenium_NO_Framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TableExcercisesFireFox {
 
-    public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "/Users/vamsik/Documents/github/Selenium_No_Framework/libs/geckodriver");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://www.cricbuzz.com/live-cricket-scorecard/18970/pak-vs-sl-2nd-t20i-pakistan-v-sri-lanka-in-uae-2017");
+    //   public static void main(String[] args) {
+    //      System.setProperty("webdriver.gecko.driver", "/Users/vamsik/Documents/github/Selenium_No_Framework/libs/geckodriver");
+    //      WebDriver driver = new FirefoxDriver();
+    //      driver.get("http://www.cricbuzz.com/live-cricket-scorecard/18970/pak-vs-sl-2nd-t20i-pakistan-v-sri-lanka-in-uae-2017");
 
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "libs/geckodriver"); //need to define where the exe is
+
+
+        driver = new FirefoxDriver();
+        driver.get("http://www.cricbuzz.com/live-cricket-scorecard/18970/pak-vs-sl-2nd-t20i-pakistan-v-sri-lanka-in-uae-2017");
 
 
     }
 
+    @AfterTest
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void sleepTest() throws InterruptedException {
+        Thread.sleep(5000);
+    }
 
 }
